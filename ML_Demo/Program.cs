38b84860ea162727209c46d641d3a5457c761653
Tomasz Falarz <t.k.falarz@gmail.com>
2019-03-26 13:52:09 +0100

    vendor taxes example

--- ML_Demo/Program.cs
+++ ML_Demo/Program.cs
@@ -1,85 +1,98 @@
-﻿using Microsoft.Data.DataView;
+﻿using System;
+using System.IO;
+using Microsoft.Data.DataView;
 using Microsoft.ML;
 using Microsoft.ML.Data;
-using System;
-
-// CS0649 compiler warning is disabled because some fields are only
-// assigned to dynamically by ML.NET at runtime
-#pragma warning disable CS0649
-
-
-// STEP 1: Define your data structures
-
-// STEP 2: Create a ML.NET environment
-
-// STEP 3: Transform your data and add a learner
-
-// STEP 4: Train your model based on the data set
-
-// STEP 5: Use your model to make a prediction
 
 
 namespace ML_Demo
 {
     class Program
     {
-        //1
-        public class IrisData
-        {
-            [LoadColumn(0)]
-            public float SepalLength;
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
 
-            [LoadColumn(1)]
-            public float SepalWidth;
-
-            [LoadColumn(2)]
-            public float PetalLength;
-
-            [LoadColumn(3)]
-            public float PetalWidth;
-
-            [LoadColumn(4)]
-            public string Label;
-        }
-
-        public class IrisPrediction
-        {
-            [ColumnName("PredictedLabel")]
-            public string PredictedLabels;
-        }
-
-                          
         static void Main(string[] args)
         {
-            //2
-            MLContext mlContext = new MLContext();
+            MLContext mlContext = new MLContext(seed: 0);
 
-            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<IrisData>(path: "iris-data.txt", hasHeader: false, separatorChar: ',');
+            var model = Train(mlContext, _trainDataPath);
 
-            //3
-            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
-                .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
-                .AppendCacheCheckpoint(mlContext)
-                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: "Label", featureColumnName: "Features"))
-                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+            Evaluate(mlContext, model);
 
-            //4!
-            var model = pipeline.Fit(trainingDataView);
+            TestSinglePrediction(mlContext);
 
-            //5
-            var prediction = model.CreatePredictionEngine<IrisData, IrisPrediction>(mlContext).Predict(
-                new IrisData()
+        }
+
+
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(dataPath, hasHeader: true, separatorChar: ',');
+
+            var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount")
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+                .Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PassengerCount", "TripTime", "TripDistance", "PaymentTypeEncoded"))
+                .Append(mlContext.Regression.Trainers.FastTree());
+
+            var model = pipeline.Fit(dataView);
+            SaveModelAsFile(mlContext, model);
+            return model;
+        }
+
+        private static void SaveModelAsFile(MLContext mlContext, ITransformer model)
+        {
+            using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                mlContext.Model.Save(model, fileStream);
+
+            Console.WriteLine("The model is saved to {0}", _modelPath);
+        }
+
+        private static void Evaluate(MLContext mlContext, ITransformer model)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(_testDataPath, hasHeader: true, separatorChar: ',');
+            var predictions = model.Transform(dataView);
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+
+            Console.WriteLine();
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Model quality metrics evaluation         ");
+            Console.WriteLine($"*------------------------------------------------");
+
+            Console.WriteLine($"*       R2 Score:      {metrics.RSquared:0.##}");
+
+            Console.WriteLine($"*       RMS loss:      {metrics.Rms:#.##}");
+        }
+
+        private static void TestSinglePrediction(MLContext mlContext)
+        {
+            ITransformer loadedModel;
+            using (var stream = new FileStream(_modelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                loadedModel = mlContext.Model.Load(stream);
+
+                var predictionFunction = loadedModel.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(mlContext);
+
+                var taxiTripSample = new TaxiTrip()
                 {
-                    SepalLength = 3.3f,
-                    SepalWidth = 1.6f,
-                    PetalLength = 0.2f,
-                    PetalWidth = 5.1f,
-                });
+                    VendorId = "VTS",
+                    RateCode = "1",
+                    PassengerCount = 1,
+                    TripTime = 1140,
+                    TripDistance = 3.75f,
+                    PaymentType = "CRD",
+                    FareAmount = 0 // To predict. Actual/Observed = 15.5
+                };
 
-            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+                var prediction = predictionFunction.Predict(taxiTripSample);
 
-            Console.WriteLine("Press any key to exit....");
-            Console.ReadLine();
+                Console.WriteLine($"**********************************************************************");
+                Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+                Console.WriteLine($"**********************************************************************");
+                Console.ReadLine();
+            }
         }
     }
 }
