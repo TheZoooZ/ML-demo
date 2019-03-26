27a46fee7e8ff2056dad2d153aebfa1e396bd48b
Tomasz Falarz <t.k.falarz@gmail.com>
2019-03-26 12:58:12 +0100

    ML-Iris-Example

---
+++ ML_Demo/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Data.DataView;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+
+// CS0649 compiler warning is disabled because some fields are only
+// assigned to dynamically by ML.NET at runtime
+#pragma warning disable CS0649
+
+
+// STEP 1: Define your data structures
+
+// STEP 2: Create a ML.NET environment
+
+// STEP 3: Transform your data and add a learner
+
+// STEP 4: Train your model based on the data set
+
+// STEP 5: Use your model to make a prediction
+
+
+namespace ML_Demo
+{
+    class Program
+    {
+        //1
+        public class IrisData
+        {
+            [LoadColumn(0)]
+            public float SepalLength;
+
+            [LoadColumn(1)]
+            public float SepalWidth;
+
+            [LoadColumn(2)]
+            public float PetalLength;
+
+            [LoadColumn(3)]
+            public float PetalWidth;
+
+            [LoadColumn(4)]
+            public string Label;
+        }
+
+        public class IrisPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabels;
+        }
+
+                          
+        static void Main(string[] args)
+        {
+            //2
+            MLContext mlContext = new MLContext();
+
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<IrisData>(path: "iris-data.txt", hasHeader: false, separatorChar: ',');
+
+            //3
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+                .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                .AppendCacheCheckpoint(mlContext)
+                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumnName: "Label", featureColumnName: "Features"))
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            //4!
+            var model = pipeline.Fit(trainingDataView);
+
+            //5
+            var prediction = model.CreatePredictionEngine<IrisData, IrisPrediction>(mlContext).Predict(
+                new IrisData()
+                {
+                    SepalLength = 3.3f,
+                    SepalWidth = 1.6f,
+                    PetalLength = 0.2f,
+                    PetalWidth = 5.1f,
+                });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+
+            Console.WriteLine("Press any key to exit....");
+            Console.ReadLine();
+        }
+    }
+}
